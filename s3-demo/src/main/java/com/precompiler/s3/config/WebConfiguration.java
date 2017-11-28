03608c57f56a2545fb7a162eb91c17a75e107f0a
Richard Li <lishengyin.msn@hotmail.com>
2017-11-28 12:14:56 +0000

    Add missing annotation.

--- s3-demo/src/main/java/com/precompiler/s3/config/WebConfiguration.java
+++ s3-demo/src/main/java/com/precompiler/s3/config/WebConfiguration.java
@@ -1,7 +1,9 @@
 package com.precompiler.s3.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@Configuration
 @EnableWebMvc
 public class WebConfiguration {
 
